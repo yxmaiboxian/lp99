fb0bc1b4c755bcd42419a79f30b211cd7846b5da
YXMAIBOXIAN <1913920561@qq.com>
2025-11-07 15:49:07 +0800

    12

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,7 +5,7 @@
     </div>
     <div v-if="show">
       <div class="box" v-for="(item, index) in imgLength" :key="item" :style="computedStyle(item)">
-        <img :style="{ 'animation-duration': computedDuration(item), 'animation-delay': 0.7 * index + 's' }"
+        <img :style="{ 'animation-duration': computedDuration(item), 'animation-delay': 1 * index + 's' }"
           class="box-img" :src="require(`@/assets/imgs/img (${item}).jpg`)" alt="">
       </div>
     </div>
@@ -26,9 +26,9 @@ export default {
     computedDuration() {
       return (item) => {
         let flag = Math.random()
-        let num = 0.4
+        let num = 0.5
         if (flag > 0.3) {
-          num = flag * 4
+          num = flag * 5
         }
         return num + 's'
       }
@@ -47,9 +47,9 @@ export default {
 
     }
   },
-  methods:{
-    changeShow(){
-      this.show=true
+  methods: {
+    changeShow() {
+      this.show = true
     }
   }
 }
@@ -60,10 +60,10 @@ export default {
   @keyframes myAnimation {
     0% {
       /* 起始状态 */
-      transform: scale(.1);
+      transform: scale(.05);
     }
 
-    70% {
+    75% {
       opacity: 0.9
     }
 
@@ -81,18 +81,18 @@ export default {
   .box {
     position: fixed;
     transform-style: preserve-3d;
-    height: 200px;
+    width: 240px;
 
     &-img {
       opacity: 0;
-      height: 100%;
+      width: 100%;
       animation: myAnimation 1s forwards;
     }
   }
 
   .btn {
-    width: 200px;
-    height: 80px;
+    width: 240px;
+    height: 100px;
     line-height: 80px;
     font-size: 30px;
     text-align: center;
@@ -104,6 +104,7 @@ export default {
     border-radius: 20px;
     cursor: pointer;
     overflow: hidden;
+
     &::before {
       content: '';
       position: absolute;
@@ -113,6 +114,7 @@ export default {
       width: 100%;
       height: 100%;
       background: linear-gradient(rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.4)), url('@/assets/imgs/img (7).jpg');
+      background-repeat: no-repeat;
       filter: blur(1px);
       background-position: center center;
       background-size: cover;
@@ -122,8 +124,7 @@ export default {
     &::after {
       content: '全球热恋';
       position: absolute;
-      font-size: 34px;
-      // font-weight: 700;
+      font-size: 40px;
       left: 50%;
       top: 50%;
       white-space: nowrap;
